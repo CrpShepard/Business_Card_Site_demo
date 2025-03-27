448edfa1ab7855f48145f8dc2023f83b8bf2b6f0
CrpShepard <54824792+CrpShepard@users.noreply.github.com>
2025-03-27 22:59:22 +0900

    добавил верификация для google search

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 app._static_folder = './src'
@@ -7,6 +7,10 @@ app._static_folder = './src'
 @app.route("/home")
 def home():
     return render_template('index.html')
+
+@app.route('/google31c1584d58f4a544.html')
+def serve_verification_file():
+    return send_from_directory('./src','google31c1584d58f4a544.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
